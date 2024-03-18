73d466b6d6f10af39285be63d9ddaf8cc523b228
DjMagic32 <garciawork3232@gmail.com>
2024-03-18 17:16:09 -0400

    fix New Client Component

--- components/sale/newClient.js
+++ components/sale/newClient.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 import {
-View, Text, StyleSheet, TextInput, TouchableOpacity, Dimensions, Image
-} from 'react-native';
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+} from "react-native";
 
-const style= StyleSheet.create({
+const style = StyleSheet.create({
   container: {
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
   },
   image: {
@@ -15,51 +21,51 @@ const style= StyleSheet.create({
   },
   title: {
     fontSize: 25,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#ED604C',
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#ED604C",
     marginVertical: 40,
   },
   textInput: {
     borderWidth: 1,
-    width: Dimensions.get('screen').width * 0.8,
-    borderColor: 'gray',
+    width: Dimensions.get("screen").width * 0.8,
+    borderColor: "gray",
     borderRadius: 10,
     marginBottom: 20,
-    textAlign: 'center',
+    textAlign: "center",
   },
   label: {
     fontSize: 13,
     marginBottom: 10,
   },
   okButton: {
-    backgroundColor: '#ED604C',
-    width: Dimensions.get('screen').width *0.5,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#ED604C",
+    width: Dimensions.get("screen").width * 0.5,
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 10,
     padding: 10,
     marginTop: 40,
   },
   okText: {
-    color: 'white',
+    color: "white",
     fontSize: 20,
-  }
+  },
 });
 
-const clientURL = 'ec2-3-84-116-82.compute-1.amazonaws.com:8000/api/v1/clientes';
+const clientURL =
+  "ec2-3-84-116-82.compute-1.amazonaws.com:8000/api/v1/clientes";
 
 const NewClient = () => {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [phone2, setPhone2] = useState("");
+  const [address, setAddress] = useState("");
+  const [nuit, setNuit] = useState("");
 
-    const [name, setName] = useState('')
-    const [phone, setPhone] = useState('')
-    const [phone, setPhone2] = useState('')
-    const [address, setAddress] = useState('')
-    const [nuit, setNuit] = useState('')
-    
-    const addClient = async() => {
-    
-      axios.post(clientURL, {
+  const addClient = async () => {
+    axios
+      .post(clientURL, {
         name: name,
         phone: phone,
         phone2: phone2,
@@ -72,37 +78,59 @@ const NewClient = () => {
       .catch(function (error) {
         console.log(error);
       });
-      clearInput()
-    }
+    clearInput();
+  };
 
-    const clearInput = () => {
-        setName('')
-        setPhone('')
-        setPhone2('')
-        setAddress('')
-        setNuit('')
-    }
+  const clearInput = () => {
+    setName("");
+    setPhone("");
+    setPhone2("");
+    setAddress("");
+    setNuit("");
+  };
 
-function NewClient({navigation}) {
-  return(
-  <View style={style.container}>
+  return (
+    <View style={style.container}>
       <Text style={style.title}>Adicionar Cliente</Text>
       <View>
         <Text style={style.label}>ESCREVA O NOME DO NEGOCIO</Text>
-        <TextInput style={style.textInput} onChangeText={setName} value={name}/>
+        <TextInput
+          style={style.textInput}
+          onChangeText={setName}
+          value={name}
+        />
         <Text style={style.label}>ESCREVA O TELEFONE DO CLIENTE *</Text>
-        <TextInput style={style.textInput} onChangeText={setPhone} value={phone}/>
+        <TextInput
+          style={style.textInput}
+          onChangeText={setPhone}
+          value={phone}
+        />
         <Text style={style.label}>ESCREVA UM TELEFONE ALTERNATIVO</Text>
-        <TextInput style={style.textInput} onChangeText={setPhone2} value={phone2}/>
+        <TextInput
+          style={style.textInput}
+          onChangeText={setPhone2}
+          value={phone2}
+        />
         <Text style={style.label}>ESCREVA A LOCALIZACAO DO CLIENTE *</Text>
-        <TextInput style={style.textInput} onChangeText={setAddress} value={address}/>
+        <TextInput
+          style={style.textInput}
+          onChangeText={setAddress}
+          value={address}
+        />
         <Text style={style.label}>ESCREVA O NUIT DO CLIENTE</Text>
-        <TextInput style={style.textInput} onChangeText={setNuit} value={nuit}/>
+        <TextInput
+          style={style.textInput}
+          onChangeText={setNuit}
+          value={nuit}
+        />
       </View>
-      <TouchableOpacity style={style.okButton} onPress= {() =>
-            navigation.navigate('menu') }>
-          <Text style={style.okText}>Ok</Text>
+      <TouchableOpacity
+        style={style.okButton}
+        onPress={() => navigation.navigate("menu")}
+      >
+        <Text style={style.okText}>Ok</Text>
       </TouchableOpacity>
-  </View>
-  )}
+    </View>
+  );
+};
 export default NewClient;
